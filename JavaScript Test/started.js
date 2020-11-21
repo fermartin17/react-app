3c56de9d70271bf072f05ee1c8d14cc69c810788
fermartin17 <femartin@itba.edu.ar>
2020-11-21 16:04:24 -0300

    initial

---
+++ JavaScript Test/started.js
@@ -0,0 +1,68 @@
+// let mayName = 'Fer';
+// console.log(mayName);
+
+
+// function printMyName(name){
+//     console.log(name);
+// }
+
+// printMyName('Fer');
+
+
+
+// const printMyName = (name) => console.log(name);
+
+
+// printMyName('Fer');
+
+// class Human {
+//     gender = 'male';
+
+//     printGender = () => {
+//         console.log(this.gender);
+//     }
+// }
+
+// class Person extends Human {
+//     name = '';
+//     constructor(name2){ 
+//         super();
+//         this.name = name2;
+//     }
+    
+//     call = () => {
+//         console.log(this.name);
+//     } 
+// }
+
+// const myPerson = new Person('Fer');
+// myPerson.call();
+// myPerson.printGender();
+
+
+
+// const numbers = [1, 2, 3];
+// const newNumbers = [...numbers,4,5];
+// console.log(newNumbers);
+
+
+// const Person = {
+//     name : 'Fer'
+// };
+
+// const newPerson = {
+//     ...Person,
+//     age : '23'
+// };
+
+// console.log(newPerson);
+
+
+// const filter = (...args) => {
+//     return args.filter(el => el === 1);
+// }
+
+// console.log(filter(1, 2, 3));
+
+
+
